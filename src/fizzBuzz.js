8a56b6c0d9cb59f48502d2057335254d1064c44d
leoncross <leon@lscross.co.uk>
2018-11-27 09:35:03 +0000

    completed fizzbuzz in js. need to further refactor, and create loop

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,5 +1,31 @@
 var Fizzbuzz = function () {};
 
+
+Fizzbuzz.prototype.says = function(number) {
+  if (this.isDivisibleByFifteen(number)) {
+    return "Fizzbuzz";
+  }
+  if (this.isDivisibleByThree(number)) {
+    return "Fizz";
+  }
+  if (this.isDivisibleByFive(number)) {
+    return "Buzz";
+  }
+  return number
+}
+
 Fizzbuzz.prototype.isDivisibleByThree = function(number) {
-  return (number % 3 === 0)
+  return this._isDivisibleBy(number, 3);
+};
+
+Fizzbuzz.prototype.isDivisibleByFive = function(number) {
+  return this._isDivisibleBy(number, 5);
+};
+
+Fizzbuzz.prototype.isDivisibleByFifteen = function(number) {
+  return this._isDivisibleBy(number, 15);
+};
+
+Fizzbuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0)
 }
